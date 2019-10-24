d9c9caee90d782af04ad16f3a95c03cf14271ac7
florianschickmair <floschi100@gmail.com>
2019-10-24 12:36:37 +0200

    create Endpoint init method

--- Centermanager/src/main/java/at/htl/centermanager/boundary/CenterManagerEndpoint.java
+++ Centermanager/src/main/java/at/htl/centermanager/boundary/CenterManagerEndpoint.java
@@ -1,4 +1,34 @@
 package at.htl.centermanager.boundary;
 
+import at.htl.centermanager.entity.Employee;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
 public class CenterManagerEndpoint {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("FahrplanEndpoint created ***");
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Employee getLinie(@PathParam("id") Long id) {
+        return em.find(Employee.class, id);
+    }
 }
+
--- Centermanager/src/main/java/at/htl/centermanager/boundary/RestConfig.java
+++ Centermanager/src/main/java/at/htl/centermanager/boundary/RestConfig.java
@@ -6,6 +6,6 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("api")
 public class RestConfig extends Application {
 
-    
+
 
 }
