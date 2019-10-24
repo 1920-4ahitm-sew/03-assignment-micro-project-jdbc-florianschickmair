9047ded8ce4bb9aae615e291799c3dd8ecee9872
florianschickmair <floschi100@gmail.com>
2019-10-24 12:29:22 +0200

    update RestConfig

--- Centermanager/src/main/java/at/htl/centermanager/boundary/RestConfig.java
+++ Centermanager/src/main/java/at/htl/centermanager/boundary/RestConfig.java
@@ -1,4 +1,11 @@
 package at.htl.centermanager.boundary;
 
-public class RestConfig {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+
+    
+
 }
