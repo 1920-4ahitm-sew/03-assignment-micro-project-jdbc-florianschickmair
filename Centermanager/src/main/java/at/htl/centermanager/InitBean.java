30ffe7c1204991c32a346f04a722e8af97bb4aa0
florianschickmair <floschi100@gmail.com>
2019-10-24 12:27:13 +0200

    add init method to InitBean

--- Centermanager/src/main/java/at/htl/centermanager/InitBean.java
+++ Centermanager/src/main/java/at/htl/centermanager/InitBean.java
@@ -1,4 +1,22 @@
 package at.htl.centermanager;
 
+import at.htl.centermanager.entity.Employee;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class InitBean {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        System.out.println("*** It works! ***");
+
+        em.persist(new Employee(1,"Schickmair"));
+        em.persist(new Employee(2,"Mair"));
+    }
 }
