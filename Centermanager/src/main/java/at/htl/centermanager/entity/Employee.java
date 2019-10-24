8753070d85462fca0bdfb9508c578da87863fb9a
florianschickmair <floschi100@gmail.com>
2019-10-24 12:21:14 +0200

    constructor getter and setter Employee

--- Centermanager/src/main/java/at/htl/centermanager/entity/Employee.java
+++ Centermanager/src/main/java/at/htl/centermanager/entity/Employee.java
@@ -1,4 +1,37 @@
 package at.htl.centermanager.entity;
 
 public class Employee {
-}
+
+    private int id;
+    private String name;
+
+    public Employee() {
+
+    }
+
+    public Employee(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
+ }
+
