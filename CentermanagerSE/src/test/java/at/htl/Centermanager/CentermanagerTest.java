5412dfadc7abfcba757920d53f3d66bb5851dd6f
florianschickmair <floschi100@gmail.com>
2019-10-25 10:33:57 +0200

    add persistence.xml

--- CentermanagerSE/src/test/java/at/htl/Centermanager/CentermanagerTest.java
+++ CentermanagerSE/src/test/java/at/htl/Centermanager/CentermanagerTest.java
@@ -1,21 +1,46 @@
 package at.htl.Centermanager;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 public class CentermanagerTest {
 
-    private Client client;
-    private WebTarget target;
 
-    @BeforeEach
+    static EntityManager em;
 
-    public void initClient(){
-        this.client = ClientBuilder.newClient();
-        this.target = client.target("https://localhost:8080/centermanager/api/");
+    @BeforeAll
+    private static void init() {
+        em = Persistence
+                .createEntityManagerFactory("myPU")
+                .createEntityManager();
+
     }
+
+    @Test
+    void test01DatabaseConnection() {
+        System.out.println("IT WORKS!");
+        Employee outboundFlight = new Flight("LNZ", "LAX", "25.10.2019");
+        Flight returnFlight = new Flight("LAX", "LNZ", "29.10.2019");
+
+        em.getTransaction().begin();
+        em.persist(outboundFlight);
+        em.persist(returnFlight);
+        returnFlight.setDate("5.11.2019");
+        em.getTransaction().commit();
+    }
+
+//    @Test
+//    void test02readOneFlight() {
+//        Flight outboundFlight = em.find(Flight.class,1L);
+//        assertThat(outboundFlight.getDeparture(),is("LNZ"));
+//        assertThat(outboundFlight.getDestination(),is("LAX"));
+//    }
 
 }
