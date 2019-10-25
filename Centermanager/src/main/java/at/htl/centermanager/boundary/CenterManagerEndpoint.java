501bd7f394b1f6657a60959a06744fd3ae96ddba
florianschickmair <floschi100@gmail.com>
2019-10-25 10:20:55 +0200

    update database

--- Centermanager/src/main/java/at/htl/centermanager/boundary/CenterManagerEndpoint.java
+++ Centermanager/src/main/java/at/htl/centermanager/boundary/CenterManagerEndpoint.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Stateless
-@Path("/")
+@Path("employee")
 public class CenterManagerEndpoint {
 
     @PersistenceContext
@@ -22,6 +22,7 @@ public class CenterManagerEndpoint {
 
     @PostConstruct
     public void init() {
+
         System.out.println("****  CenterManagerEP created ***");
     }
     @GET
